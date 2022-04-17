d720b4ea232580f3494cdc009242b27e8026b240
Dennis Chow <dennischow@Denniss-MBP>
2022-04-17 10:17:34 +0800

    Moved Navigation as a component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import { Routes, Route } from "react-router-dom";
 
+import Navgiation from "./routes/navgiation/navgiation.component"
 import Home from "./routes/home/home.component";
+
+
+
+const Shop = () => {
+    return (
+        <h1>I am the shop page</h1>
+    );
+};
 
 const App = () => {
     return (
         <Routes>
-            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Navgiation />}>
+                <Route index element={<Home />} />
+                <Route path="/shop" element={<Shop />} />
+            </Route>
         </Routes>
     );
 };
---
+++ src/routes/shop/ahop.component.jsx
@@ -0,0 +1,9 @@
+
+
+const Shop = () => {
+    return (
+        <div />
+    );
+};
+
+export default Shop;
